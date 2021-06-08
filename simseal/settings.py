4e3f72a1032beb5782f2cf305a2d2c6c1c44d213
吴迪 <wud@hsmap.com>
2021-06-08 13:30:25 +0800

    image bug fix

--- simseal/settings.py
+++ simseal/settings.py
@@ -163,15 +163,15 @@ else:
     STATIC_ROOT = str(BASE_DIR / 'static')
 
 
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (os.path.join('static'), )
+# STATICFILES_DIRS = (os.path.join('static'), )
 
 # simleui configs
 SIMPLEUI_CONFIG = {
--- simseal/urls.py
+++ simseal/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.views.static import serve
 from django.views.generic import RedirectView
 
 from apps.simc.views import CharacterView, CardView, MonsterView
@@ -30,4 +33,11 @@ urlpatterns = [
     path('api/monster/', MonsterView.as_view()),
     url(r'^favicon\.ico$', RedirectView.as_view(
         url='/static/images/favicon.ico')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT
+        }),
+    ]
